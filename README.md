9908c457550cd0f90ccc537c226418d7759a291b
Umur Gedik <umurgdk@gmail.com>
2013-01-27 00:02:01 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-grunt-snockets
-==============
+# grunt-snockets
 
-Grunt.js plugin for snockets.js
+Snockets for grunt.js
+
+## Getting Started
+Install this grunt plugin next to your project's [grunt.js gruntfile][getting_started] with: `npm install grunt-snockets`
+
+Then add this line to your project's `grunt.js` gruntfile:
+
+```javascript
+grunt.loadNpmTasks('grunt-snockets');
+```
+
+[grunt]: http://gruntjs.com/
+[getting_started]: https://github.com/gruntjs/grunt/blob/master/docs/getting_started.md
+
+## Documentation
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt][grunt].
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Umur Gedik  
+Licensed under the MIT license.
---
+++ tasks/snockets.js
@@ -0,0 +1,51 @@
+var Snockets = require('snockets'),
+    path = require('path');
+
+/*
+ * grunt-snockets
+ * https://github.com/umurgedik/grunt-snockets
+ *
+ * Copyright (c) 2013 Umur Gedik
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+  var snockets = new Snockets();
+
+  // Please see the grunt documentation for more information regarding task and
+  // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
+
+  // ==========================================================================
+  // TASKS
+  // ==========================================================================
+
+  grunt.registerTask('snockets', 'Building js files with snockets.js.', function() {
+    // It doesn't run with empty src and dest parameters.    
+    if (typeof this.data.src === 'undefined' ||
+        typeof this.data.dest === 'undefined') {
+      grunt.log.error('Missing Options: src and dest options necessary');
+      return false;
+    }
+
+    if (fs.existsSync(path.resolve(src))) {
+      js = snockets.getConcatenation(src, {async: false});
+
+      fs.writeFileSync(path.resolve(dest), js);
+
+      grunt.log.write(this.data.src + ' snocket to ' this.data.dest);
+      return true;
+    } else {
+      grunt.log.error('Missing File: ' + this.data.src);
+      return false;
+    }
+  });
+
+  // ==========================================================================
+  // HELPERS
+  // ==========================================================================
+
+  grunt.registerHelper('snockets', function() {
+    return 'snockets!!!';
+  });
+
+};
